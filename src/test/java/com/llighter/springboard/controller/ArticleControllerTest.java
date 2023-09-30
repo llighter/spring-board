169d5538a9621b02a27240ab7e1a250d1535f102
Park, Yunha <hadbsgk@gmail.com>
2023-09-30 23:27:51 +0900

    #15 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러 테스트 그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@Import` 설정 추가

--- src/test/java/com/llighter/springboard/controller/ArticleControllerTest.java
+++ src/test/java/com/llighter/springboard/controller/ArticleControllerTest.java
@@ -1,20 +1,20 @@
 package com.llighter.springboard.controller;
 
+import com.llighter.springboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
