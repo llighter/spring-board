9567382dbe29ec3395a7def2dc475ea0e77e8b2f
Park, Yunha <hadbsgk@gmail.com>
2023-09-30 22:42:50 +0900

    #15 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/llighter/springboard/controller/ArticleController.java
+++ src/main/java/com/llighter/springboard/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.llighter.springboard.controller;
 
+import com.llighter.springboard.domain.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,5 +18,12 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{article_id}")
+    public String article(@PathVariable Long article_id, ModelMap map) {
+        map.addAttribute("article", Article.of("제목", "내용", "해시태그")); // TODO: 실제 데이터로 대체
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
